3fdf7311e9b9d3acd2aecaa538d386798c2b70e3
luky116 <luky116@126.com>
2024-10-09 15:55:39 +0800

    修改 arm 编译问题

--- console/src/main/java/com/alibaba/nacos/Nacos.java
+++ console/src/main/java/com/alibaba/nacos/Nacos.java
@@ -32,6 +32,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Nacos {
     
     public static void main(String[] args) {
+        System.setProperty("nacos.home", "/Users/oker/meili/yuecai.liu_dacs_at_okg.com/114/Desktop/nacos/data");
         SpringApplication.run(Nacos.class, args);
     }
 }

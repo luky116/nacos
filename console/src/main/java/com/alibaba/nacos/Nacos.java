2e6a7e83c6c87444138f1ab091e7c59ec9f5f956
luky116 <luky116@126.com>
2024-10-09 16:13:42 +0800

    更新编译信息

--- console/src/main/java/com/alibaba/nacos/Nacos.java
+++ console/src/main/java/com/alibaba/nacos/Nacos.java
@@ -32,6 +32,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Nacos {
     
     public static void main(String[] args) {
+        System.setProperty("nacos.home", "");
         SpringApplication.run(Nacos.class, args);
     }
 }

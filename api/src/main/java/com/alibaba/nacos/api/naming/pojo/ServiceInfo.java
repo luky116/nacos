5f8a97e831495b0df59ffcafa53910ceae1b1651
yuecai.liu <yuecai.liu@okg.com>
2025-02-26 11:42:24 +0800

    fix processServiceInfo bug

--- api/src/main/java/com/alibaba/nacos/api/naming/pojo/ServiceInfo.java
+++ api/src/main/java/com/alibaba/nacos/api/naming/pojo/ServiceInfo.java
@@ -212,14 +212,17 @@ public class ServiceInfo {
         String serviceName = getGroupedServiceName();
         return getKey(serviceName, clusters);
     }
+
+    @JsonIgnore
+    public String getKeyWithoutClusters() {
+        return getGroupedServiceName();
+    }
     
     @JsonIgnore
     public static String getKey(String name, String clusters) {
-        
         if (!isEmpty(clusters)) {
             return name + Constants.SERVICE_INFO_SPLITER + clusters;
         }
-        
         return name;
     }
     
